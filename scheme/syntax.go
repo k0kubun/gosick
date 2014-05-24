ecf52fc54d16eeea1d251497d9996917f300b728
Takashi Kokubun <takashikkbn@gmail.com>
2014-05-24 16:09:36 +0900

    Update code order

--- scheme/syntax.go
+++ scheme/syntax.go
@@ -6,6 +6,127 @@ import (
 	"fmt"
 )
 
+type Cond struct {
+	ObjectBase
+	cases    []Object
+	elseBody Object
+}
+
+func NewCond(parent Object) *Cond {
+	return &Cond{ObjectBase: ObjectBase{parent: parent}}
+}
+
+func (c *Cond) Eval() Object {
+	for _, caseBody := range c.cases {
+		elements := caseBody.(*Pair).Elements()
+		lastResult := elements[0].Eval()
+
+		if lastResult.isBoolean() && lastResult.(*Boolean).value == false {
+			continue
+		}
+
+		for _, element := range elements {
+			lastResult = element.Eval()
+		}
+		return lastResult
+	}
+
+	if c.elseBody == nil {
+		return undef
+	}
+
+	elements := c.elseBody.(*Pair).Elements()
+	lastResult := Object(undef)
+	for _, element := range elements {
+		lastResult = element.Eval()
+	}
+	return lastResult
+}
+
+type Do struct {
+	ObjectBase
+	iterators    []*Iterator
+	testBody     Object
+	continueBody Object
+	localBinding Binding
+}
+
+func NewDo(parent Object) *Do {
+	return &Do{ObjectBase: ObjectBase{parent: parent}, localBinding: Binding{}}
+}
+
+func (d *Do) binding() Binding {
+	return d.localBinding
+}
+
+func (d *Do) scopedBinding() Binding {
+	scopedBinding := make(Binding)
+	for identifier, object := range d.localBinding {
+		scopedBinding[identifier] = object
+	}
+
+	parent := d.Parent()
+	for parent != nil {
+		for identifier, object := range parent.binding() {
+			if scopedBinding[identifier] == nil {
+				scopedBinding[identifier] = object
+			}
+		}
+		parent = parent.Parent()
+	}
+	return scopedBinding
+}
+
+func (d *Do) Eval() Object {
+	// bind iterators
+	for _, iterator := range d.iterators {
+		if iterator.variable.isVariable() {
+			d.localBinding[iterator.variable.(*Variable).identifier] = iterator.value.Eval()
+		}
+	}
+
+	// eval test ->
+	//   true: eval testBody and returns its result
+	//  false: eval continueBody, eval iterator's update
+	testElements := d.testBody.(*Pair).Elements()
+	continueElements := d.continueBody.(*Pair).Elements()
+	for {
+		testResult := testElements[0].Eval()
+		if !testResult.isBoolean() || testResult.(*Boolean).value == true {
+			for _, element := range testElements[1:] {
+				testResult = element.Eval()
+			}
+			return testResult
+		} else {
+			// eval continueBody
+			for _, element := range continueElements {
+				element.Eval()
+			}
+
+			// update iterators
+			for _, iterator := range d.iterators {
+				if iterator.variable.isVariable() {
+					d.localBinding[iterator.variable.(*Variable).identifier] = iterator.update.Eval()
+				}
+			}
+		}
+	}
+	return undef
+}
+
+type Iterator struct {
+	ObjectBase
+	variable Object
+	value    Object
+	update   Object
+}
+
+func NewIterator(parent Object) *Iterator {
+	return &Iterator{ObjectBase: ObjectBase{parent: parent}}
+}
+
+// *** upper from here is legacy code ***
+
 var (
 	builtinSyntaxes = Binding{
 		"set!":  NewSyntax(setSyntax),
@@ -129,123 +250,4 @@ func beginSyntax(s *Syntax, arguments Object) Object {
 		lastResult = object.Eval()
 	}
 	return lastResult
-}
-
-type Cond struct {
-	ObjectBase
-	cases    []Object
-	elseBody Object
-}
-
-func NewCond(parent Object) *Cond {
-	return &Cond{ObjectBase: ObjectBase{parent: parent}}
-}
-
-func (c *Cond) Eval() Object {
-	for _, caseBody := range c.cases {
-		elements := caseBody.(*Pair).Elements()
-		lastResult := elements[0].Eval()
-
-		if lastResult.isBoolean() && lastResult.(*Boolean).value == false {
-			continue
-		}
-
-		for _, element := range elements {
-			lastResult = element.Eval()
-		}
-		return lastResult
-	}
-
-	if c.elseBody == nil {
-		return undef
-	}
-
-	elements := c.elseBody.(*Pair).Elements()
-	lastResult := Object(undef)
-	for _, element := range elements {
-		lastResult = element.Eval()
-	}
-	return lastResult
-}
-
-type Do struct {
-	ObjectBase
-	iterators    []*Iterator
-	testBody     Object
-	continueBody Object
-	localBinding Binding
-}
-
-func NewDo(parent Object) *Do {
-	return &Do{ObjectBase: ObjectBase{parent: parent}, localBinding: Binding{}}
-}
-
-func (d *Do) binding() Binding {
-	return d.localBinding
-}
-
-func (d *Do) scopedBinding() Binding {
-	scopedBinding := make(Binding)
-	for identifier, object := range d.localBinding {
-		scopedBinding[identifier] = object
-	}
-
-	parent := d.Parent()
-	for parent != nil {
-		for identifier, object := range parent.binding() {
-			if scopedBinding[identifier] == nil {
-				scopedBinding[identifier] = object
-			}
-		}
-		parent = parent.Parent()
-	}
-	return scopedBinding
-}
-
-func (d *Do) Eval() Object {
-	// bind iterators
-	for _, iterator := range d.iterators {
-		if iterator.variable.isVariable() {
-			d.localBinding[iterator.variable.(*Variable).identifier] = iterator.value.Eval()
-		}
-	}
-
-	// eval test ->
-	//   true: eval testBody and returns its result
-	//  false: eval continueBody, eval iterator's update
-	testElements := d.testBody.(*Pair).Elements()
-	continueElements := d.continueBody.(*Pair).Elements()
-	for {
-		testResult := testElements[0].Eval()
-		if !testResult.isBoolean() || testResult.(*Boolean).value == true {
-			for _, element := range testElements[1:] {
-				testResult = element.Eval()
-			}
-			return testResult
-		} else {
-			// eval continueBody
-			for _, element := range continueElements {
-				element.Eval()
-			}
-
-			// update iterators
-			for _, iterator := range d.iterators {
-				if iterator.variable.isVariable() {
-					d.localBinding[iterator.variable.(*Variable).identifier] = iterator.update.Eval()
-				}
-			}
-		}
-	}
-	return undef
-}
-
-type Iterator struct {
-	ObjectBase
-	variable Object
-	value    Object
-	update   Object
-}
-
-func NewIterator(parent Object) *Iterator {
-	return &Iterator{ObjectBase: ObjectBase{parent: parent}}
 }
